8bab379eefa3c4879b42f86c38a47cc00c3a6b36
Naima Malik <noma_m@hotmail.com>
2023-10-24 14:24:31 +0200

    Get users answers and submit answers

--- src/component/InputComponent.jsx
+++ src/component/InputComponent.jsx
@@ -9,6 +9,9 @@ export const InputComponent = () => {
   const [answer, setAnswer] = useState("");
   const [isAnswerFilled, setIsAnswerFilled] = useState(false);
 
+  const [userResponses, setUserResponses] = useState([]);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
   const questions = data[currentQuestion];
 
   const handleNextQuestion = () => {
@@ -16,6 +19,7 @@ export const InputComponent = () => {
       if (currentQuestion < data.length - 1) {
         setQuestions(currentQuestion + 1);
         setIsAnswerFilled(false); // it resets the answer filled status
+        setUserResponses([...userResponses, answer]);
         setAnswer(""); // Clears the answer
       }
     } else {
@@ -34,59 +38,84 @@ export const InputComponent = () => {
     setRangeValue(selectedValue);
     setIsAnswerFilled(selectedValue !== "0");
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+    setUserResponses([...userResponses, answer]);
+  };
   return (
-    <div className="form-container">
-      <div key={questions.id}>
-        <p>{questions.question}</p>
-      </div>
-      {questions.type === "text" && (
-        <input type="text" onChange={handleChange} />
-      )}
-      {questions.type === "select" && (
-        <select value={answer} onChange={handleChange}>
-          {questions.options.map((option, index) => (
-            <option key={index} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-      )}
-      {questions.type === "radio" && (
-        <div>
-          {questions.options.map((option, index) => (
-            <div key={index}>
-              <input
-                type="radio"
-                value={option}
-                name={`radioOption_${questions.id}`}
-                onChange={handleChange}
-              />
-              <label>{option}</label>
+    <>
+      {!isSubmitted && (
+        <div className="form-container">
+          <div key={questions.id}>
+            <p>{questions.question}</p>
+          </div>
+          {questions.type === "text" && (
+            <input type="text" onChange={handleChange} />
+          )}
+          {questions.type === "select" && (
+            <select value={answer} onChange={handleChange}>
+              {questions.options.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          )}
+          {questions.type === "radio" && (
+            <div>
+              {questions.options.map((option, index) => (
+                <div key={index}>
+                  <input
+                    type="radio"
+                    value={option}
+                    name={`radioOption_${questions.id}`}
+                    onChange={handleChange}
+                  />
+                  <label>{option}</label>
+                </div>
+              ))}
             </div>
-          ))}
+          )}
+
+          {questions.type === "range" && (
+            <>
+              <input
+                type="range"
+                min={questions.min}
+                max={questions.max}
+                step={questions.step}
+                value={rangeValue}
+                onChange={handleRangeChange}
+              />
+              <label htmlFor="rangeInput">{rangeValue}</label>
+            </>
+          )}
+          <div>
+            {currentQuestion < data.length - 1 ? (
+              <button onClick={handleNextQuestion}>Next</button>
+            ) : (
+              <button onClick={handleSubmit}>Submit</button>
+            )}
+          </div>
         </div>
       )}
 
-      {questions.type === "range" && (
-        <>
-          <input
-            type="range"
-            min={questions.min}
-            max={questions.max}
-            step={questions.step}
-            value={rangeValue}
-            onChange={handleRangeChange}
-          />
-          <label htmlFor="rangeInput">{rangeValue}</label>
-        </>
+      {userResponses && (
+        <div>
+          {!isSubmitted && <h2>User's Answers:</h2> ? (
+            isSubmitted
+          ) : (
+            <div className="answers">
+              <h2>User's Answers:</h2>
+              {userResponses.map((response, index) => {
+                return <p key={index}>{response}</p>;
+              })}
+            </div>
+          )}
+        </div>
       )}
-      <div>
-        {currentQuestion < data.length - 1 ? (
-          <button onClick={handleNextQuestion}>Next</button>
-        ) : (
-          <button>Submit</button>
-        )}
-      </div>
-    </div>
+    </>
   );
 };
